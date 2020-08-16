2c3770e400f12199f9238baaf0e86e54e9e83154
吴广靓 <v-wuguangliang@xiaomi.com>
2020-08-16 20:57:25 +0800

    [NewFeature] 新增demo
    Signed-off-by: 吴广靓 <v-wuguangliang@xiaomi.com>

---
+++ Demo/app/src/main/java/com/dimens_auto/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dimens_auto.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // see res/values/dimens_auto.xml
+        // todo show diff
+    }
+}
